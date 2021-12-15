8e80cd954d5f87466122c683f65d90304288e367
RuiOkazaki <okazaki200226@gmail.com>
2021-12-15 16:11:27 +0900

    add: header complete

---
+++ m1/S/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="shadow-lg">
+    <h1 class="text-4xl">Tokyo Big Sizer</h1>
+    <ul class="text-lg">
+      <li><router-link to="/">トップページ</router-link></li>
+      <li><router-link to="/artist">アーティスト一覧</router-link></li>
+      <li><router-link to="/news">ニュース</router-link></li>
+    </ul>
+  </header>
+</template>
+<style scoped>
+header {
+  width: 100%;
+  height: 100px;
+  padding-inline: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* box-shadow: ; */
+}
+ul {
+  display: flex;
+  gap: 24px;
+}
+</style>
